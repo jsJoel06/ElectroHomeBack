91cf52c108aa7db4d5242beabd0965ecb2b3042a
Joel0612 <https://github.com/Joel0612/Venta.git>
2026-02-07 13:28:19 -0400

    Commit -m "algunas modificaciones"

--- src/main/java/com/empleos/electrohome/controllers/ImagenController.java
+++ src/main/java/com/empleos/electrohome/controllers/ImagenController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/imagenes")
+@CrossOrigin(origins = "*")
 public class ImagenController {
 
     private final Path root = Paths.get("uploads");

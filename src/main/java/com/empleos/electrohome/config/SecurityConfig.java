dffcdaa1ee716ddd0e62c98b64bdc260817208d2
Joel0612 <https://github.com/Joel0612/Venta.git>
2026-02-06 22:17:12 -0400

    Commit -m "algunas modificaciones"

--- src/main/java/com/empleos/electrohome/config/SecurityConfig.java
+++ src/main/java/com/empleos/electrohome/config/SecurityConfig.java
@@ -38,20 +38,18 @@ public class SecurityConfig implements WebMvcConfigurer {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
+                        // Permitimos las fotos y la API de lectura sin login
                         .requestMatchers("/uploads/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/productos/**", "/api/categorias/**").permitAll()
-                        .requestMatchers("/error").permitAll() // <--- AGREGA ESTA LÍNEA
 
+                        // Protegemos el AddForm (Guardar/Editar)
                         .requestMatchers(HttpMethod.POST, "/api/productos/**").hasAuthority("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/productos/**").hasAuthority("ADMIN")
+
                         .anyRequest().authenticated()
                 )
-                // IMPORTANTE: Reactiva Basic Auth para que el AddForm pueda enviar tus credenciales ADMIN
-                .httpBasic(Customizer.withDefaults())
-
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )

a30c4975c7428e2b6d8c894fc62fd29fce5db066
Joel0612 <https://github.com/Joel0612/Venta.git>
2026-02-06 22:59:22 -0400

    Commit -m "algunas modificaciones"

--- src/main/java/com/empleos/electrohome/config/WebConfig.java
+++ src/main/java/com/empleos/electrohome/config/WebConfig.java
@@ -1,10 +1,8 @@
 package com.empleos.electrohome.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.nio.file.Paths;
 
 @Configuration
@@ -12,8 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Obtenemos la ruta absoluta de la carpeta 'uploads' en el servidor de Render
+        String uploadPath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:./uploads/") // El './' fuerza la ruta relativa a la raíz
-                .setCachePeriod(0);
+                .addResourceLocations(uploadPath)
+                .setCachePeriod(0); // Para que los cambios se vean al instante sin caché
+
+        // Esto saldrá en los logs de Render para confirmar que encontró la carpeta
+        System.out.println("SISTEMA NUBE: Servidor buscando fotos en: " + uploadPath);
     }
 }

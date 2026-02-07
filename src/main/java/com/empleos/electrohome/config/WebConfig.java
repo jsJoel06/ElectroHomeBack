c2e67549684a31612f417e550514db0c80e954b7
Joel0612 <https://github.com/Joel0612/Venta.git>
2026-02-06 22:37:13 -0400

    Commit -m "algunas modificaciones"

--- src/main/java/com/empleos/electrohome/config/WebConfig.java
+++ src/main/java/com/empleos/electrohome/config/WebConfig.java
@@ -12,13 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Esta ruta funciona en local y en la nube
-        // Spring buscará la carpeta 'uploads' que está al lado de tu pom.xml o jar
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
-
-        // Log para confirmar la ruta en los logs de Render
-        String absolutePath = new java.io.File("uploads").getAbsolutePath();
-        System.out.println("SISTEMA ACTIVO: Servidor buscando fotos en: " + absolutePath);
+                .addResourceLocations("file:./uploads/") // El './' fuerza la ruta relativa a la raíz
+                .setCachePeriod(0);
     }
 }
